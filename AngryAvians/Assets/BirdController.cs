186f3015cdf697c7f3b10365534dbd7c1fa1194a
Nguyen Duong <ducnguyenduchao@gmail.com>
2020-08-17 21:36:15 -0700

    broken slingshot

---
+++ AngryAvians/Assets/BirdController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BirdController : MonoBehaviour
+{
+    [SerializeField] 
+    private float releaseTime;
+
+    private Rigidbody2D rb;
+    private SpringJoint2D springJoint;
+
+    private bool isMouseDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        springJoint = GetComponent<SpringJoint2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown((int)MouseButton.LeftMouse))
+        {
+            isMouseDown = true;
+        }
+        
+        if(Input.GetMouseButtonUp((int)MouseButton.LeftMouse)) {
+            StartCoroutine(Release(releaseTime));
+            isMouseDown = false;
+        }
+
+        if(isMouseDown)
+        {
+            rb.MovePosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+        }
+    }
+
+    private IEnumerator Release(float time)
+    {
+        yield return new WaitForSeconds(time);
+        springJoint.enabled = false;
+        this.enabled = false;
+    }
+    
+}
