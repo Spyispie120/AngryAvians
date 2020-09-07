ccd92ea279b4c8fba37f59d89eae5c238e3d74f5
Nguyen Duong <ducnguyenduchao@gmail.com>
2020-09-07 00:12:23 -0700

    bird code change

--- AngryAvians/Assets/Resources/Scripts/BirdController.cs
+++ AngryAvians/Assets/Resources/Scripts/BirdController.cs
@@ -8,28 +8,22 @@ using UnityEngine.SceneManagement;
 
 public class BirdController : MonoBehaviour
 {
-    [SerializeField] 
-    private float releaseTime;
 
-    private Rigidbody2D rb;
-    private SpringJoint2D springJoint;
-    [SerializeField]
-    private Transform anchor;
-    private SpriteRenderer sr;
+    protected SpriteRenderer sr;
 
-    private bool isMouseDown;
-    private bool holdBird;
+    public Rigidbody2D RB { get; private set; }
+    public SpringJoint2D SpringJoint { get; private set; }
 
-    private const string DIRECTORY = "Sprites/Avians/DefaultAvian/";
-    [SerializeField] private Sprite defaultSprite;
-    [SerializeField] private Sprite flyingSprite;
-    [SerializeField] private float MAX_DISTANCE;
+    public const string DIRECTORY = "Sprites/Avians/DefaultAvian/";
+    [SerializeField] protected Sprite defaultSprite;
+    [SerializeField] protected Sprite flyingSprite;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        springJoint = GetComponent<SpringJoint2D>();
+        RB = GetComponent<Rigidbody2D>();
+        SpringJoint = GetComponent<SpringJoint2D>();
         //anchor = springJoint.attachedRigidbody.transform.GetChild(0);
 
         sr = this.transform.GetChild(0).GetComponentInChildren<SpriteRenderer>();
@@ -43,68 +37,14 @@ public class BirdController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
 
-        if(Input.GetMouseButtonDown((int)MouseButton.LeftMouse))
-        {
-            isMouseDown = true;
-        }
-        
-        if(Input.GetMouseButtonUp((int)MouseButton.LeftMouse)) {
-            rb.isKinematic = false;
-            isMouseDown = false;
-            
-            if(holdBird) StartCoroutine(Release(releaseTime));
-            holdBird = false;
-            
-        }
-
-        if(isMouseDown && holdBird)
-        {
-            Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 connectedAnchorWorldPos = anchor.position;// transform.TransformPoint(springJoint.connectedAnchor);
-            Debug.Log(connectedAnchorWorldPos);
-            Vector2 mouseDir = (mouseWorldPos - connectedAnchorWorldPos).normalized;
-            float mouseLength = Mathf.Abs(Vector2.Distance(mouseWorldPos, connectedAnchorWorldPos));//springJoint.distance;//
-
-            Vector2 movePoint = mouseLength <= MAX_DISTANCE ? 
-                                mouseWorldPos : 
-                                connectedAnchorWorldPos + mouseDir * MAX_DISTANCE;
-
-            Debug.DrawLine(connectedAnchorWorldPos, movePoint, Color.cyan, 1);
-
-            rb.MovePosition(movePoint);
-            rb.isKinematic = true;
-        }
     }
 
-    private void FixedUpdate()
+    public void Release()
     {
-        RaycastHit2D rayHit;
-
-        if(isMouseDown)
-        {
-            rayHit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.back);
-            if(rayHit.collider != null)
-            {
-                if(rayHit.collider.CompareTag("Bird"))
-                {
-                    holdBird = true;
-                }
-            }
-        }
-    }
-
-    private IEnumerator Release(float time)
-    {
-        yield return new WaitForSeconds(time);
-        springJoint.enabled = false;
         sr.sprite = flyingSprite;
-        //this.enabled = false;
     }
+
+
     
 }
--- AngryAvians/Assets/Resources/Scripts/Slingshot.cs
+++ AngryAvians/Assets/Resources/Scripts/Slingshot.cs
@@ -1,20 +1,113 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class Slingshot : MonoBehaviour
 {
     private LineRenderer line;
 
+    [SerializeField]
+    private List<BirdController> birds;
+    private BirdController currentBird;
+
+    [SerializeField] private float MAX_DISTANCE;
+
+    [SerializeField]
+    private Transform anchor;
+
+    [SerializeField]
+    private float releaseTime;
+
+    private bool isMouseDown;
+    private bool holdBird;
+
+    private Rigidbody2D rb;
+    private SpringJoint2D springJoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentBird = birds[0];
+        rb = currentBird.RB;
+        springJoint = currentBird.SpringJoint;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if (Input.GetMouseButtonDown((int)MouseButton.LeftMouse))
+        {
+            isMouseDown = true;
+        }
+
+
         
+        if (Input.GetMouseButtonUp((int)MouseButton.LeftMouse))
+        {
+            rb.isKinematic = false;
+            isMouseDown = false;
+
+            // Release
+            if (holdBird)
+            {
+
+                StartCoroutine(Release(releaseTime));
+            }
+            holdBird = false;
+
+        }
+
+        if (isMouseDown && holdBird)
+        {
+            Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 connectedAnchorWorldPos = anchor.position;// transform.TransformPoint(springJoint.connectedAnchor);
+            Debug.Log(connectedAnchorWorldPos);
+            Vector2 mouseDir = (mouseWorldPos - connectedAnchorWorldPos).normalized;
+            float mouseLength = Mathf.Abs(Vector2.Distance(mouseWorldPos, connectedAnchorWorldPos));//springJoint.distance;//
+
+            Vector2 movePoint = mouseLength <= MAX_DISTANCE ?
+                                mouseWorldPos :
+                                connectedAnchorWorldPos + mouseDir * MAX_DISTANCE;
+
+            //Debug.DrawLine(connectedAnchorWorldPos, movePoint, Color.cyan, 1);
+
+            rb.MovePosition(movePoint);
+            rb.isKinematic = true;
+        }
+
     }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D rayHit;
+
+        if (isMouseDown)
+        {
+            rayHit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.back);
+            if (rayHit.collider != null)
+            {
+                if (rayHit.collider.CompareTag("Bird"))
+                {
+                    holdBird = true;
+                }
+            }
+        }
+    }
+
+    private IEnumerator Release(float time)
+    {
+        yield return new WaitForSeconds(time);
+        springJoint.enabled = false;
+        
+        //this.enabled = false;
+    }
+
 }
